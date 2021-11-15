6c972be8405d970ada87e72716d1f1435b9d6e62
Shreyas Bhat <bshreyas@vt.edu>
2021-11-15 13:08:54 -0500

    Updated conditions for file check

--- preprocess_prepare_data.py
+++ preprocess_prepare_data.py
@@ -81,8 +81,10 @@ if __name__ == '__main__':
        
         for video_name in video_list :
             vidframe_folder = video_name.split('.')[0]
-            if video_name in processed_roi or video_name in skip_list and vidframe_folder not in incomp_processed_frames :
-                continue
+            
+            if video_name in processed_roi or video_name in skip_list:
+                if vidframe_folder not in incomp_processed_frames :
+                    continue
             video = os.path.join(data_path,folder,video_name)
             with open('log_processed.txt', 'a') as file:
                         file.write("%s\n" %video_name )
