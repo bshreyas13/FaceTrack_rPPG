c64c9347cf23f86f25bc28ea275233597bb526ee
Shreyas Bhat <bshreyas@vt.edu>
2021-11-22 00:08:04 -0500

    testing datagen

--- prepare_data.py
+++ prepare_data.py
@@ -13,26 +13,24 @@ import re
 import tensorflow as tf
 from modules.videodatasethandler import VideoDatasetHandler
 from modules.preprocessor import Preprocessor 
-## write getSubset before calling generator 
 
-in_data = ['s11_trial27']
-labels_save_path =  (os.path.join(os.path.dirname(os.getcwd()),'Labels'))
-roi = os.path.join(os.path.dirname(os.getcwd()),'Dataset' , 'Roi')
-data_path = roi
-labels_path = labels_save_path
+## The script gets subset of data , splits data to obtain train , val, test sets ##
+## Then calls datagen to obtain samples ##
+## calls train_test_plot to train and evaluate model ##
+
+import os
+from modules.videodatasethandler import VideoDatasetHandler
+from modules.preprocessor import Preprocessor
+
 vdh = VideoDatasetHandler()
 p = Preprocessor()
-# test = tf.data.Dataset.from_generator( dataGenerator,args = ['DeepPhys', in_data,roi ,labels_save_path],output_types=(tf.int8,tf.float32),output_shapes=((215,300,3), ()))
 
-# dataset = tf.data.Dataset.from_generator(our_generator, (tf.float32, tf.int16))
-
-# for batch, (x,y) in enumerate(test):
-  
-#     print("Data shape: ", x.shape, y.shape)
-
-a =  p.loadData(os.path.join(labels_save_path,in_data[0]+'.dat'))
-n = vdh.dataGenerator('FaceTrack_rPPG', in_data,roi ,labels_save_path)
-for k in range (1):    
-    i,y = next(n)
-    print(i.shape)
-    print(y.shape)
+in_data = ['s11_trial27']
+labels_path =  (os.path.join(os.path.dirname(os.getcwd()),'Labels'))
+roi = os.path.join(os.path.dirname(os.getcwd()),'Dataset' , 'Roi')
+datagen = vdh.dataGenerator('FaceTrack_rPPG', in_data,roi ,labels_path)
+    
+x,y = next(datagen)
+print(x.shape)
+print(y.shape)
+    
