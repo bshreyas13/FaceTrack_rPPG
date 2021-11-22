c024fc632285dc5a0011b1f801055397669748a1
Shreyas Bhat <bshreyas@vt.edu>
2021-11-22 15:19:35 -0500

    bug fix

--- prepare_data.py
+++ prepare_data.py
@@ -32,19 +32,19 @@ def getDatasets(model, appearance_path,motion_path, labels_path, x_shape, y_shap
     train_ds = tf.data.Dataset.from_generator(
         generator = datagen_train, 
         output_types=({np.float64,np.float64}, np.float64), 
-        output_shapes=([x_shape,x_shape], y_shape))
+        )
     
     datagen_val = vdh.dataGenerator(model, val_set, appearance_path, motion_path, labels_path, batch_size =50, timesteps = 5 , img_size = (300,215,3))
     val_ds = tf.data.Dataset.from_generator(
         generator = datagen_val, 
         output_types=({np.float64,np.float64}, np.float64), 
-        output_shapes=([x_shape,x_shape], y_shape))
+        )
 
     datagen_test= vdh.dataGenerator(model, test_set, appearance_path, motion_path, labels_path, batch_size =50, timesteps = 5 , img_size = (300,215,3))
     test_ds= tf.data.Dataset.from_generator(
         generator = datagen_test, 
         output_types=({np.float64,np.float64}, np.float64), 
-        output_shapes=([x_shape,x_shape], y_shape))
+        )
     
     return train_ds, val_ds, test_ds
 
