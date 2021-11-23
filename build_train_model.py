afebf1ffab6aa78ebb3a248b3445ecd5a2780b3a
Shreyas Bhat <bshreyas@vt.edu>
2021-11-23 18:30:42 -0500

    model.fit_generator test

--- build_train_model.py
+++ build_train_model.py
@@ -67,7 +67,7 @@ def train_test_plot(model,optimizer, train_ds,val_ds,test_ds,epochs,batch_size):
   callbacks = [checkpoint, lr_reducer, lr_scheduler]
   
   # Train the model 
-  history= model.fit(train_ds,
+  history= model.fit_generator(train_ds,
                         validation_data=val_ds,
                         epochs=epochs, verbose=1, workers=4,
                         callbacks=callbacks)
