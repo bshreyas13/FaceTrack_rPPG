d7e5f24fab5e0494081adcd097bb579202da7809
Shreyas Bhat <bshreyas@vt.edu>
2021-11-23 14:59:09 -0500

    tuple (inpt,inpt),(output) format

--- prepare_data.py
+++ prepare_data.py
@@ -23,24 +23,27 @@ def getDatasets(model, appearance_path,motion_path, labels_path, x_shape, y_shap
     
     train_set, val_set, test_set = getSets(motion_path,subset,val_split,test_split)
     vdh = VideoDatasetHandler()
+    
+    types=((tf.float64, tf.float64), tf.float64)
+    shapes = ({x_shape, x_shape}, y_shape)
     ## Train, Val, Test Dataset for Appearance Stream
     datagen_train = vdh.dataGenerator(model, train_set, appearance_path , motion_path, labels_path, batch_size =50, timesteps = 5 , img_size = (300,215,3))
     train_ds = tf.data.Dataset.from_generator(
         generator = datagen_train, 
-        output_types=({'input_1':tf.float64,'input_2':tf.float64}, tf.float64), 
-        output_shapes = ({'input_1':x_shape,'input_2':x_shape}, y_shape))
+        output_types= types, 
+        output_shapes = shapes)
     
     datagen_val = vdh.dataGenerator(model, val_set, appearance_path, motion_path, labels_path, batch_size =50, timesteps = 5 , img_size = (300,215,3))
     val_ds = tf.data.Dataset.from_generator(
         generator = datagen_val, 
-        output_types=({'input_1':tf.float64,'input_2':tf.float64}, tf.float64), 
-        output_shapes = ({'input_1':x_shape,'input_2':x_shape}, y_shape))
+        output_types=types, 
+        output_shapes = shapes)
 
     datagen_test= vdh.dataGenerator(model, test_set, appearance_path, motion_path, labels_path, batch_size =50, timesteps = 5 , img_size = (300,215,3))
     test_ds= tf.data.Dataset.from_generator(
         generator = datagen_test, 
-        output_types=({'input_1':tf.float64,'input_2':tf.float64}, tf.float64), 
-        output_shapes = ({'input_1':x_shape,'input_2':x_shape}, y_shape))
+        output_types=types, 
+        output_shapes = shapes)
     
     return train_ds, val_ds, test_ds
 
