741b62656182e0aaccb17313cb3c28d6bdb277e5
bshreyas13 <bshreyas@vt.edu>
2021-12-07 12:25:16 -0500

    bug fix for data check

--- build_train_model_v2.py
+++ build_train_model_v2.py
@@ -126,9 +126,11 @@ if __name__ == '__main__':
             elif os.isdir(os.path.join(motion_path,folder)):
                 shutil.rmtree(os.path.join(motion_path,folder))
         for folder in incomp_motion:
-            if folder not in appearance_path:
-                shutil.rmtree(os.path.join(appearance_path,folder))
-                shutil.rmtree(os.path.join(motion_path,folder))
+            if folder not in incomp_appearance:
+                if os.isdir(os.path.join(appearance_path,folder)):
+                    shutil.rmtree(os.path.join(appearance_path,folder))
+                elif os.isdir(os.path.join(motion_path,folder)):
+                    shutil.rmtree(os.path.join(motion_path,folder))
         ap_list = os.listdir(appearance_path)
         print("No of data folders :{} Appearance ,{} Motion".format(len(appearance_path),len(motion_path)))
     ## Check for txt file and tfrecord paths
