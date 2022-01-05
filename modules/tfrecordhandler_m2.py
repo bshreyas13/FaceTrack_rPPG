7a432d2f2f4b04b0c514882d1419d50c817bcce0
Shreyas <bshreyas@vt.edu>
2022-01-05 15:49:34 -0500

    testing DeepPhys without compression in tfrecord

--- modules/tfrecordhandler_m2.py
+++ modules/tfrecordhandler_m2.py
@@ -217,7 +217,7 @@ class TFRReader():
         motion_image = tf.io.decode_raw(parsed_ex['Motion'], tf.uint8)
         motion_image = tf.reshape(motion_image, shape=(im_height, im_width, im_depth))
         
-        appearance_image = tf.io.decode_jpeg(parsed_ex['Appearance'], tf.uint8)
+        appearance_image = tf.io.decode_raw(parsed_ex['Appearance'], tf.uint8)
         appearance_image = tf.reshape(appearance_image, shape=(im_height, im_width, im_depth))
         
         label = tf.cast(parsed_ex['Labels'], dtype = tf.int32)
