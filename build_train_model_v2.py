5e92eb01cc70ed0fa718b811df061f39953accad
Shreyas <bshreyas@vt.edu>
2022-01-18 23:00:42 -0500

    Testing updated attention masks

--- build_train_model_v2.py
+++ build_train_model_v2.py
@@ -36,15 +36,15 @@ from modules.videodatasethandler import VideoDatasetHandler
 ##Learning Rate Schedule ##
 def lr_schedule(epoch):
 
-    lr = 0.1e-1
+    lr = 1e-2
     if epoch > 80:
        lr *= 1e-1
     elif epoch > 60:
-        lr *= 0.5e-1
+        lr *= 0.5
     elif epoch > 40:
         lr *= 1e-1
     elif epoch > 10:
-        lr *= 0.5e-1
+        lr *= 0.5
         
     print('Learning rate: ', lr)
     return lr
