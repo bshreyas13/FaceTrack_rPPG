71df6e49ac8fb991ab4e7c837f7bb6a464be47b5
Shreyas <bshreyas@vt.edu>
2022-01-21 03:28:00 -0500

    Test Predict

--- modules/tfrecordhandler_DP.py
+++ modules/tfrecordhandler_DP.py
@@ -244,7 +244,7 @@ class TFRReader():
         appearance_image = tf.io.decode_jpeg(parsed_ex['Appearance'], channels=3)
         appearance_image = tf.reshape(appearance_image, shape=(im_height, im_width, im_depth))
         
-        label = tf.cast(parsed_ex['Labels'], dtype = tf.int32)
+        label = tf.cast(parsed_ex['Labels'], dtype = tf.float32)
         
         return (motion_image, appearance_image), label, im_name, frames
     
--- predict.py
+++ predict.py
@@ -6,12 +6,29 @@ if __name__ == '__main__':
     
     parser = ap.ArgumentParser()
     parser.add_argument("-mp","--model_path", required = True , help = "Path to metrics file")
-    parser.add_argument("-mp","--model_name", required = True , help = "DeepPhys/FaceTrack_rPPG")
+    parser.add_argument("-tfr_path","--tfrecord_path", required = True , help = "Path to metrics file")
+    parser.add_argument("-mn","--model_name", required = True , help = "DeepPhys/FaceTrack_rPPG")
+
     args = vars(parser.parse_args())
      
     ## Get args
     m_path = args["model_path"]
-	train_ds,val_ds,test_ds = prep.getDatasets(model_name,appearance_path,motion_path,labels_path,txt_files_paths,tfrecord_path, batch_size=batch_size, timesteps=timesteps, subset=subset, subset_read = subset_train, val_split = val_split , test_split =test_split,write_txt_files=wtxt, create_tfrecord=wtfr,rot=1)
+    tfrecord_path =args["tfrecord_path"]
+    model_name = args["model_name"]
+	split = 'example'
+	appearance_path = ""
+	motion_path = ""
+	labels_path = ""
+	txt_files_paths = ""
+	batch_size = 1
+	timesteps = 5
+	subset = 0.02
+	subset_train = 1
+	wtfr = False
+	wtxt = False
+	val_split = 0.1
+	test_split = 0.2
+	#train_ds,val_ds,test_ds = prep.getDatasets(model_name,appearance_path,motion_path,labels_path,txt_files_paths,tfrecord_path, batch_size=batch_size, timesteps=timesteps, subset=subset, subset_read = subset_train, val_split = val_split , test_split =test_split,write_txt_files=wtxt, create_tfrecord=wtfr,rot=0)
     model = load_model(m_path)
 
     if model_name == "FaceTrack_rPPG":
@@ -23,9 +40,10 @@ if __name__ == '__main__':
 
     #tfwrite = TFRWriter()
 
-    #tfwrite.writeTFRecords(roi_path,nd_path, txt_files_path, tfrecord_path, file_list, batch_size,split,timesteps )
-    tfrpath = os.path.join(tfrecord_path.as_posix(),split)
+    tfrpath = os.path.join(tfrecord_path,split)
     # make a dataset iterator
     data = TFRReader(batch_size, timesteps)
 
-    model.predict
+
+    signal= model.predict(data,batch_size = 2)
+    print(len(signal))
