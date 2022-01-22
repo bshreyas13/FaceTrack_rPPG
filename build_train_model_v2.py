d42b1d93ca435578822d05eb43b76cd3f7aea823
Shreyas <bshreyas@vt.edu>
2022-01-22 14:18:45 -0500

    Updating spatial average method, updated final dense layer

--- build_train_model_v2.py
+++ build_train_model_v2.py
@@ -36,7 +36,7 @@ from modules.preprocessor import Preprocessor
 ##Learning Rate Schedule ##
 def lr_schedule(epoch):
 
-    lr = 0.1e-2
+    lr = 1e-2
     if epoch > 80:
        lr *= 1e-1
     elif epoch > 60:
