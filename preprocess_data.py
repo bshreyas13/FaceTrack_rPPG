5ea8875c63e57ba51e92750bb38bd9a1193966ef
Shreyas <bshreyas@vt.edu>
2022-01-22 21:35:17 -0500

    Testinf updates

--- preprocess_data.py
+++ preprocess_data.py
@@ -98,8 +98,9 @@ if __name__ == '__main__':
         print("Using {}% videos for each subject".format(subset*100))
         ## Get normalized difference frame  
         data_folders = os.listdir(data_path)
+        print(data_folders)
         print(len(data_folders))
-        sys.exist()
+        sys.exit()
         for folder in tqdm(data_folders):
             video_list = os.listdir(os.path.join(data_path,folder))
             video_list = vdh.getSubset(video_list,subset)
