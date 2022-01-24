0a589202bbd48dd8d65655ec625d51b1c4908537
Shreyas <bshreyas@vt.edu>
2022-01-24 01:05:38 -0500

    Updated optimizer

--- build_train_model_v2.py
+++ build_train_model_v2.py
@@ -295,7 +295,7 @@ if __name__ == '__main__':
             
         input_shape = (timesteps,img_size[0],img_size[1],img_size[2])
         #optimizer = Adam(learning_rate=lr_schedule(0))
-        optimizer = Adadelta(learning_rate=lr_schedule(0))
+        optimizer = SGD(learning_rate=lr_schedule(0))
         if tpu == True:
             with tpu_strategy.scope(): # creating the model in the TPUStrategy scope means we will train the model on the TPU
   
